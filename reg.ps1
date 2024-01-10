5c8dd951d98e5bd924e192307282b3aa77854dc2
Daniel Gumprecht <daniel.gumprecht@gmx.at>
2024-01-10 11:50:01 +0100

    Update reg.ps1

--- reg.ps1
+++ reg.ps1
@@ -1,2 +1,12 @@
+Install-WindowsFeature Search-Service
+Install-WindowsFeature Telnet-Client
+Install-WindowsFeature TFTP-Client
+Add-WindowsFeature Windows-Server-Backup -IncludeManagementTools -Restart
+Enable-WindowsOptionalFeature -Online -FeatureName "NetFx3" -All
+n
+
+Enable-WindowsOptionalFeature -Online -FeatureName "SMB1Protocol" -All
+n
+
 Uninstall-WindowsFeature -Name Windows-Defender
 shutdown /r /t 0
