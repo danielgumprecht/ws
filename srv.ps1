0a689c477067499022b4a6bd01b61a873c55c306
Daniel Gumprecht <daniel.gumprecht@gmx.at>
2024-01-11 08:44:35 +0100

    Update srv.ps1

--- srv.ps1
+++ srv.ps1
@@ -9,7 +9,3 @@ Enable-WindowsOptionalFeature -Online -FeatureName "SMB1Protocol" -All
 n
 
 Uninstall-WindowsFeature -Name Windows-Defender
-
-New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\NlaSvc\Parameters" -Name "AlwaysExpectDomainController" -Value 1 -Type DWORD
-
-shutdown /r /t 0
