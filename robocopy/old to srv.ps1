7224360d179540d0bde38ac3abbff3180e6804e5
Daniel Gumprecht <daniel.gumprecht@gmx.at>
2024-03-28 00:04:06 +0100

    Update old to srv.ps1

--- robocopy/old to srv.ps1
+++ robocopy/old to srv.ps1
@@ -8,12 +8,15 @@ New-Item -ItemType Directory -Path "\\$srv\clientapps$\_Backups\$env:USERNAME\Mo
 
 # Edge > SRV
 Get-Process -Name msedge -ErrorAction SilentlyContinue | Stop-Process -Force
+New-Item  -ItemType Directory -Path "\\$srv\clientapps$\_Backups\$env:USERNAME\Edge"
 robocopy "\Users\$env:USERNAME\AppData\Local\Microsoft\Edge" "\\$srv\clientapps$\_Backups\$env:USERNAME\Edge" /E /MIR /W:0 /V
 
 # Chrome > SRV
 Get-Process -Name chrome -ErrorAction SilentlyContinue | Stop-Process -Force
+New-Item  -ItemType Directory -Path "\\$srv\clientapps$\_Backups\$env:USERNAME\Google"
 robocopy "\Users\$env:USERNAME\AppData\Local\Google" "\\$srv\clientapps$\_Backups\$env:USERNAME\Google" /E /MIR /W:0 /V
 
 # Firefox > SRV
 Get-Process -Name firefox -ErrorAction SilentlyContinue | Stop-Process -Force
+New-Item  -ItemType Directory -Path "\\$srv\clientapps$\_Backups\$env:USERNAME\Mozilla"
 robocopy "\Users\$env:USERNAME\AppData\Local\Mozilla" "\\$srv\clientapps$\_Backups\$env:USERNAME\Mozilla" /E /MIR /W:0 /V
