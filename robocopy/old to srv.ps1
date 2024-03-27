edad07c173c676d0861fef7952b97722dec0743f
Daniel Gumprecht <daniel.gumprecht@gmx.at>
2024-03-28 00:22:04 +0100

    change folder name to browser

--- robocopy/old to srv.ps1
+++ robocopy/old to srv.ps1
@@ -2,15 +2,15 @@ $srv = Read-Host "enter SRV IP"
 
 # Edge > SRV
 Get-Process -Name msedge -ErrorAction SilentlyContinue | Stop-Process -Force
-New-Item -ItemType Directory -Path "\\$srv\clientapps$\_Backups\$env:USERNAME\Edge"
-robocopy "$env:localappdata\Microsoft\Edge" "\\$srv\clientapps$\_Backups\$env:USERNAME\Edge" /E /MIR /W:0 /V
+New-Item -ItemType Directory -Path "\\$srv\clientapps$\_Browser\$env:USERNAME\Edge"
+robocopy "$env:localappdata\Microsoft\Edge" "\\$srv\clientapps$\_Browser\$env:USERNAME\Edge" /E /MIR /W:0 /V
 
 # Chrome > SRV
 Get-Process -Name chrome -ErrorAction SilentlyContinue | Stop-Process -Force
-New-Item -ItemType Directory -Path "\\$srv\clientapps$\_Backups\$env:USERNAME\Chrome"
-robocopy "$env:localappdata\Google\Chrome" "\\$srv\clientapps$\_Backups\$env:USERNAME\Chrome" /E /MIR /W:0 /V
+New-Item -ItemType Directory -Path "\\$srv\clientapps$\_Browser\$env:USERNAME\Chrome"
+robocopy "$env:localappdata\Google\Chrome" "\\$srv\clientapps$\_Browser\$env:USERNAME\Chrome" /E /MIR /W:0 /V
 
 # Firefox > SRV
 Get-Process -Name firefox -ErrorAction SilentlyContinue | Stop-Process -Force
-New-Item -ItemType Directory -Path "\\$srv\clientapps$\_Backups\$env:USERNAME\Firefox"
-robocopy "$env:localappdata\Mozilla\Firefox" "\\$srv\clientapps$\_Backups\$env:USERNAME\Firefox" /E /MIR /W:0 /V
+New-Item -ItemType Directory -Path "\\$srv\clientapps$\_Browser\$env:USERNAME\Firefox"
+robocopy "$env:localappdata\Mozilla\Firefox" "\\$srv\clientapps$\_Browser\$env:USERNAME\Firefox" /E /MIR /W:0 /V
--- robocopy/srv to new.ps1
+++ robocopy/srv to new.ps1
@@ -7,12 +7,12 @@ robocopy "\\$srv\profile$\$env:USERNAME.V6\Downloads" "\Users\$env:USERNAME\Down
 
 # Copy Edge
 Get-Process -Name msedge -ErrorAction SilentlyContinue | Stop-Process -Force
-robocopy "\\$srv\clientapps$\_Backups\$env:USERNAME\Edge" "$env:localappdata\Microsoft\Edge" /E /MIR /W:0 /V
+robocopy "\\$srv\clientapps$\_Browser\$env:USERNAME\Edge" "$env:localappdata\Microsoft\Edge" /E /MIR /W:0 /V
 
 # Copy Chrome
 Get-Process -Name chrome -ErrorAction SilentlyContinue | Stop-Process -Force
-robocopy "\\$srv\clientapps$\_Backups\$env:USERNAME\Chrome" "$env:localappdata\Google\Chrome" /E /MIR /W:0 /V
+robocopy "\\$srv\clientapps$\_Browser\$env:USERNAME\Chrome" "$env:localappdata\Google\Chrome" /E /MIR /W:0 /V
 
 # Copy Firefox
 Get-Process -Name firefox -ErrorAction SilentlyContinue | Stop-Process -Force
-robocopy "\\$srv\clientapps$\_Backups\$env:USERNAME\Firefox" "$env:localappdata\Mozilla\Firefox" /E /MIR /W:0 /V
+robocopy "\\$srv\clientapps$\_Browser\$env:USERNAME\Firefox" "$env:localappdata\Mozilla\Firefox" /E /MIR /W:0 /V
