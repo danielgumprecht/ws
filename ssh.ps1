8128a1f566e4842e4f2f644549df5e76b98274d1
Daniel Gumprecht <daniel.gumprecht@gmx.at>
2024-03-27 21:22:51 +0100

    ssh enable script

---
+++ ssh.ps1
@@ -0,0 +1,3 @@
+Start-Service sshd
+Set-Service -Name sshd -StartupType 'Automatic'
+New-NetFirewallRule -Name sshd -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
