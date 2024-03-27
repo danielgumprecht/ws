3822e2aa73130d30bdd7f16d9bdbe2f1e31f1734
Daniel Gumprecht <daniel.gumprecht@gmx.at>
2024-03-28 00:20:26 +0100

    path changes and optimizing length with variables

--- robocopy/old to srv.ps1
+++ robocopy/old to srv.ps1
@@ -3,14 +3,14 @@ $srv = Read-Host "enter SRV IP"
 # Edge > SRV
 Get-Process -Name msedge -ErrorAction SilentlyContinue | Stop-Process -Force
 New-Item -ItemType Directory -Path "\\$srv\clientapps$\_Backups\$env:USERNAME\Edge"
-robocopy "\Users\$env:USERNAME\AppData\Local\Microsoft\Edge" "\\$srv\clientapps$\_Backups\$env:USERNAME\Edge" /E /MIR /W:0 /V
+robocopy "$env:localappdata\Microsoft\Edge" "\\$srv\clientapps$\_Backups\$env:USERNAME\Edge" /E /MIR /W:0 /V
 
 # Chrome > SRV
 Get-Process -Name chrome -ErrorAction SilentlyContinue | Stop-Process -Force
-New-Item -ItemType Directory -Path "\\$srv\clientapps$\_Backups\$env:USERNAME\Google"
-robocopy "\Users\$env:USERNAME\AppData\Local\Google" "\\$srv\clientapps$\_Backups\$env:USERNAME\Google" /E /MIR /W:0 /V
+New-Item -ItemType Directory -Path "\\$srv\clientapps$\_Backups\$env:USERNAME\Chrome"
+robocopy "$env:localappdata\Google\Chrome" "\\$srv\clientapps$\_Backups\$env:USERNAME\Chrome" /E /MIR /W:0 /V
 
 # Firefox > SRV
 Get-Process -Name firefox -ErrorAction SilentlyContinue | Stop-Process -Force
-New-Item -ItemType Directory -Path "\\$srv\clientapps$\_Backups\$env:USERNAME\Mozilla"
-robocopy "\Users\$env:USERNAME\AppData\Local\Mozilla" "\\$srv\clientapps$\_Backups\$env:USERNAME\Mozilla" /E /MIR /W:0 /V
+New-Item -ItemType Directory -Path "\\$srv\clientapps$\_Backups\$env:USERNAME\Firefox"
+robocopy "$env:localappdata\Mozilla\Firefox" "\\$srv\clientapps$\_Backups\$env:USERNAME\Firefox" /E /MIR /W:0 /V
