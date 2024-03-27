59c92c822ebe3b4090fcc44ded3bdc279f3d2bbb
Daniel Gumprecht <daniel.gumprecht@gmx.at>
2024-03-27 21:42:07 +0100

    Update rmu.ps1

--- rmu.ps1
+++ rmu.ps1
@@ -2,9 +2,9 @@
 $username = Read-Host "Enter the username to delete"
 
 # Delete user from registry
-Remove-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\$username" -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\$username" -Force
 
 # Delete user folder
-Remove-Item -Path "C:\Users\$username" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Folder -Path "C:\Users\$username" -Recurse -Force
 
 Write-Host "User $username has been deleted."
