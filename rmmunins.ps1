72604ba6af64fef5aab7d01659c745e261a0c21b
Daniel Gumprecht <daniel.gumprecht@gmx.at>
2024-03-29 20:50:51 +0100

    Create rmmunins.ps1

---
+++ rmmunins.ps1
@@ -0,0 +1,25 @@
+# kill N-Able services
+$tasksToKill = @("PME.Agent", "FileCacheServiceAgent", "RequestHandlerAgent", "ASupSrvc", "BASupSrvcUpdater", "winagent")
+
+foreach ($task in $tasksToKill) {
+    Get-Process -Name $task -ErrorAction SilentlyContinue | Stop-Process -Force -ErrorAction Continue
+}
+
+# disable N-Able services
+$services = @(
+    "PME.Agent.PmeService",
+    "SolarWinds.MSP.CacheService",
+    "SolarWinds.MSP.RpcServerService",
+    "BASupportExpressStandaloneService_LOGICnow",
+    "BASupportExpressSrvcUpdater_LOGICnow",
+    "Advanced Monitoring Agent"
+
+foreach ($service in $services) {
+    Set-Service -Name $service -StartupType Disabled -ErrorAction Stop
+}
+
+# Remove N-able install locations, also removes them from services.msc
+Remove-Item 'C:\Program Files (x86)\Advanced Monitoring Agent GP\' -recurse -force
+Remove-Item 'C:\Program Files (x86)\Advanced Monitoring Agent\' -recurse -force
+Remove-Item 'C:\Program Files (x86)\Take Control Agent\' -recurse -force
+
