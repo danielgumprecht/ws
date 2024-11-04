40232f2842a0e1840a78ce840657e9b3c26ac2e6
Daniel Gumprecht <daniel.gumprecht@gmx.at>
2024-11-05 00:19:41 +0100

    Update nlafix.ps1

--- nlafix.ps1
+++ nlafix.ps1
@@ -16,6 +16,3 @@ Enable-NetFirewallRule -DisplayGroup "Datei- und Druckerfreigabe"
 
 # set Dependencies for NLA
 sc.exe config nlasvc depend= NSI/RpcSs/TcpIp/Dhcp/Eventlog/DNS
-
-# Disable IPv6
-Disable-NetAdapterBinding -Name * -ComponentID "ms_tcpip6"
