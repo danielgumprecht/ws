c45099318b0b33a03c36b98e754555787375673c
Daniel Gumprecht <daniel.gumprecht@gmx.at>
2026-06-01 21:41:33 +0200

    Update fix.ps1

--- fix.ps1
+++ fix.ps1
@@ -1,9 +1,9 @@
-Write-Output "Executing system checks. This may take some time..."
+Write-Output "Executing system checks. This may take some time..." -ForegroundColor Cyan
 chkdsk /scan /perf
-Write-Output "Running chkdsk completed. Now running SFC and DISM to check for system integrity issues."
+Write-Output "Running chkdsk completed. Now running SFC and DISM to check for system integrity issues." -ForegroundColor Cyan
 sfc /scannow
-Write-Output "SFC scan completed. Now running DISM to check for component store corruption."
+Write-Output "SFC scan completed. Now running DISM to check for component store corruption." -ForegroundColor Cyan
 dism /online /cleanup-image /restorehealth
-Write-Output "DISM scan completed. Now running SFC again to ensure all issues are resolved."
+Write-Output "DISM scan completed. Now running SFC again to ensure all issues are resolved." -ForegroundColor Cyan
 sfc /scannow
-Write-Output "System check complete. Please review the results above for any issues."
+Write-Output "System check complete. Please review the results above for any issues." -ForegroundColor Green
