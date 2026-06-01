d0e03c49d9ec08542246eda0a4b1a38dabbe97b9
Daniel Gumprecht <daniel.gumprecht@gmx.at>
2026-06-01 21:39:05 +0200

    comments and upgrade

--- fix.ps1
+++ fix.ps1
@@ -1,4 +1,9 @@
+Write-Output "Executing system checks. This may take some time..."
 chkdsk /scan /perf
+Write-Output "Running chkdsk completed. Now running SFC and DISM to check for system integrity issues."
 sfc /scannow
+Write-Output "SFC scan completed. Now running DISM to check for component store corruption."
 dism /online /cleanup-image /restorehealth
+Write-Output "DISM scan completed. Now running SFC again to ensure all issues are resolved."
+sfc /scannow
 Write-Output "System check complete. Please review the results above for any issues."
--- upgrade.ps1
+++ upgrade.ps1
@@ -1,4 +1,4 @@
 setup.exe /auto upgrade /showoobe none /dynamicupdate disable /eula accept
 
 cd 'C:\$WINDOWS.~BT\Sources\Panther\'
-Get-Content .\setupact.log -tail 50 -wait
+Get-Content .\setupact.log -tail 50 -waitWrite-Output "System check complete. Please review the results above for any issues."
