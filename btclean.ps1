eafb2c10a7900ea52facc52987f31b9f2c1e4b2b
Daniel Gumprecht <daniel.gumprecht@gmx.at>
2026-06-01 21:26:59 +0200

    Update btclean.ps1

--- btclean.ps1
+++ btclean.ps1
@@ -1,3 +1,3 @@
-takeown /f C:\$WINDOWS.~BT /r /d y
-icacls C:\$WINDOWS.~BT /grant administrators:F /t
-Remove-Item /s /q C:\$WINDOWS.~BT
+takeown /F 'C:\$WINDOWS.~BT\' /R /D y
+icacls 'C:\$WINDOWS.~BT\' /grant administrators:F /t
+Remove-Item /s /q 'C:\$WINDOWS.~BT\'
