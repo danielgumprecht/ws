41533c6736e7603b38592281a68c43ea563b00a5
Daniel Gumprecht <daniel.gumprecht@gmx.at>
2026-06-01 21:45:40 +0200

    adding comments and fixing upgrade

--- pnp.ps1
+++ pnp.ps1
@@ -1,3 +1,8 @@
+Write-Output "Removing non-present PnP devices." -ForegroundColor Cyan
+
+
 Get-PnpDevice | Where-Object { $_.Present -eq $false } | ForEach-Object {
     pnputil /remove-device "$($_.InstanceId)"
 }
+
+Write-Output "Non-present PnP devices removed. Please review the output above for details." -ForegroundColor Green
--- upgrade.ps1
+++ upgrade.ps1
@@ -1,4 +1,6 @@
-setup.exe /auto upgrade /showoobe none /dynamicupdate disable /eula accept
+Write-Output "Starting Windows upgrade process. This may take some time, please be patient." -ForegroundColor Cyan
+.\setup.exe /auto upgrade /showoobe none /dynamicupdate disable /eula accept
 
+Write-Output "Upgrade process initiated. Please follow any on-screen prompts and do not interrupt the process." -ForegroundColor Green
 cd 'C:\$WINDOWS.~BT\Sources\Panther\'
 Get-Content .\setupact.log -tail 50 -wait
