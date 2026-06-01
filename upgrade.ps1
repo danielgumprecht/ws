47cdeae6352d8e2abe93de05c2e1acc665ee8ed1
Daniel Gumprecht <daniel.gumprecht@gmx.at>
2026-06-02 00:19:31 +0200

    change something

--- upgrade.ps1
+++ upgrade.ps1
@@ -1,6 +1,9 @@
 Write-Host "Starting Windows upgrade process. This may take some time, please be patient." -ForegroundColor Cyan
 .\setup.exe /auto upgrade /showoobe none /dynamicupdate disable /eula accept
 
+Start-Sleep -Seconds 10
+
 Write-Host "Upgrade process initiated. Please follow any on-screen prompts and do not interrupt the process." -ForegroundColor Green
 cd 'C:\$WINDOWS.~BT\Sources\Panther\'
 Get-Content .\setupact.log -tail 50 -wait
+Start-Sleep -Seconds 10
