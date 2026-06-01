c7c958f404598abbebf8e27ed9b3973f8aa175aa
Daniel Gumprecht <daniel.gumprecht@gmx.at>
2026-06-01 20:19:33 +0200

    Create utils.ps1

---
+++ utils.ps1
@@ -0,0 +1,4 @@
+winget install lostindark.driverstoreexplorer
+winget install bleachbit.bleachbit
+winget install microsoft.sysinternals.autoruns
+winget install microsoft.sysinternals.processexplorer
