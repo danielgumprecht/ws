32a51b21c10c7c4f6531e40569a22d1b50c91200
Daniel Gumprecht <daniel.gumprecht@gmx.at>
2026-06-03 13:27:13 +0200

    add onenote printer for deletion

--- pnp.ps1
+++ pnp.ps1
@@ -5,4 +5,20 @@ Get-PnpDevice | Where-Object { $_.Present -eq $false } | ForEach-Object {
     pnputil /remove-device "$($_.InstanceId)"
 }
 
+Get-PnpDevice -FriendlyName OneNote -ErrorAction SilentlyContinue | ForEach-Object {
+    pnputil /remove-device "$($_.InstanceId)"
+}
+
+Get-PnpDevice -FriendlyName "OneNote for Windows 10" -ErrorAction SilentlyContinue | ForEach-Object {
+    pnputil /remove-device "$($_.InstanceId)"
+}
+
+Get-PnpDevice -FriendlyName "OneNote (Desktop)" -ErrorAction SilentlyContinue | ForEach-Object {
+    pnputil /remove-device "$($_.InstanceId)"
+}
+
+Get-PnpDevice -FriendlyName "An OneNote 16 senden" -ErrorAction SilentlyContinue | ForEach-Object {
+    pnputil /remove-device "$($_.InstanceId)"
+}
+
 Write-Host "Non-present PnP devices removed. Please review the output above for details." -ForegroundColor Green
